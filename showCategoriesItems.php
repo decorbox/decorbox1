ec360faa8162dac9eaf6619f7d4da9c44f2a9608
MindaugasA <mindaugaskramb@gmail.com>
2015-02-18 14:44:55 +0200

    added product pagination

--- showCategoriesItems.php
+++ showCategoriesItems.php
@@ -16,38 +16,69 @@ while ($title = mysqli_fetch_array($get_cat_title_res)){
 	$cat_title = $title['cat_title'];	
 	}
 
+//pagination
 
-//pagination	
-//get total number of record
-$get_total_num_sql="SELECT id FROM store_items WHERE cat_id = '".$safe_cat_id."'";
-$get_total_num = mysqli_query($mysqli, $get_total_num_sql) or die(mysql_error($mysqli));
-$row = mysqli_fetch_array($get_total_num);
-$rec_count = $row[0];
-$rec_limit = 5;//itemu kiekis, nerodo pirmo itemo kazkodel
+//gets current page number
+$items_per_page = 1;
 if(isset($_GET['page'])){
-	$page = $_GET['page'] + 1;
-	$offset = $rec_limit * $page;
+	$current_page = $_GET['page'];
 }else{
-	$page = 0;
-	$offset = 0;
+	$current_page = 1;
 }
-$left_rec = $rec_count - ($page * $rec_limit);
+//calculates the offset for sql LIMIT
+$offset = $items_per_page * ($current_page - 1);
 
-//get items
+//gets items based on page
 $get_items_sql = "SELECT id, item_title, item_price, item_image FROM store_items WHERE cat_id = '".$safe_cat_id."' order by item_title";
-$query_limit_view = sprintf("%s LIMIT %d, %d", $get_items_sql, $row, $rec_limit);
+$query_limit_view = sprintf("%s LIMIT %d, %d", $get_items_sql, $offset, $items_per_page);
 $get_items_res = mysqli_query($mysqli, $query_limit_view) or die(mysqli_error($mysqli));
-//$get_items_res = mysqli_query($mysqli, $get_items_sql) or die(mysqli_error($mysqli));
-//end of pagination
 
+//counts how manny items and pages are in the db
+$get_total_num_sql="SELECT COUNT(*) AS total FROM store_items WHERE cat_id = '".$safe_cat_id."'";
+$query = mysqli_query($mysqli, $get_total_num_sql) or die(mysql_error($mysqli));
+$result = mysqli_fetch_array($query);
+$total = $result['total'];
+$total_pages = (int)($total / $items_per_page); 
 
-$pagination = "<ul class='pagination'>
-  <li><a href='#''>1</a></li>
-  <li><a href='#'>2</a></li>
-  <li><a href='#''>3</a></li>
-  <li><a href='#'>4</a></li>
-  <li><a href='#'>5</a></li>
-</ul>";
+//form the page link
+$url = $_SERVER['PHP_SELF'] . "?cat_id=" . $safe_cat_id;
+
+//displays pagination only if there is more than 1 page
+$pagination = '';
+$amount_of_numbered_pages = 8;
+if ($total_pages > 1) {
+	//pagination header
+	$pagination .= '<div class="row"><div class="col-md-12"><ul class="pagination">';
+	//displays first and previous arrows if the current page is not the first
+	if ($current_page > 1) {
+		$pagination .= '<li><a href="' . $url . '&page=' . 1 . '">' . '<<' . '</a></li>';
+		$pagination .= '<li><a href="' . $url . '&page=' . ($current_page - 1) . '">' . '<' . '</a></li>';
+		$page_counter_start = $current_page - 1;
+	} else {
+		$pagination .= '<li class="disabled"><a href="#">' . '<<' . '</a></li>';
+		$pagination .= '<li class="disabled"><a href="#">' . '<' . '</a></li>';
+		$page_counter_start = $current_page;
+	}
+	//displays 8 numbered pages, 1 previous, 1 current and 6 next
+	for ($page_counter = $page_counter_start, $i = 0; $page_counter <= $total_pages && $i < $amount_of_numbered_pages; $page_counter++, $i++) {
+		if ($page_counter != $current_page) {
+			$pagination .= '<li><a href="' . $url . '&page=' . $page_counter . '">' . $page_counter . '</a></li>';
+		//marks the current page as active
+		} else {
+			$pagination .= '<li class="active"><a href="' . $url . '&page=' . $page_counter . '">' . $page_counter . '</a></li>';
+		}
+	}
+	//displays next and last arrows if the current page is not the last
+	if ($current_page != $total_pages) {
+		$pagination .= '<li><a href="' . $url . '&page=' . ($current_page + 1) . '">' . '>' . '</a></li>';
+		$pagination .= '<li><a href="' . $url . '&page=' . $total_pages . '">' . '>>' . '</a></li>';
+	} else {
+		$pagination .= '<li class="disabled"><a href="#">' . '>' . '</a></li>';
+		$pagination .= '<li class="disabled"><a href="#">' . '>>' . '</a></li>';
+	}
+	//pagination footer
+	$pagination .= '</ul></div></div>';
+}
 //end of pagination
 
 $display_block = "<h1>$cat_title</h1>";
